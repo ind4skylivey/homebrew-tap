ac8b2dd3c25c5aadeb0bd81800b00529d592b73f
il1v3y <il1v3y@users.noreply.github.com>
2025-11-27 22:11:49 +0000

    [DOCS] Update materiatrack to 1.0.4

--- Formula/materiatrack.rb
+++ Formula/materiatrack.rb
@@ -1,11 +1,11 @@
-# Homebrew formula for MateriaTrack
+# Homebrew formula for MatteriaTrack
 # Final Fantasy-themed CLI time tracker
 
 class Materiatrack < Formula
   desc "Mystical Final Fantasy-themed CLI time tracker based on Zeit"
   homepage "https://github.com/ind4skylivey/matteria-track"
-  url "https://github.com/ind4skylivey/matteria-track/archive/refs/tags/v1.0.3.tar.gz"
-  sha256 "8a51681613fcb885868d9e0c35c321c29c5660d0be2cc15811a4de6509874327"
+  url "https://github.com/ind4skylivey/matteria-track/archive/refs/tags/v1.0.4.tar.gz"
+  sha256 "92cb8bf4f1a6073bff61839cd146208417541ec4f5673cb18482cc4b3e5dbc37"
   license "MIT"
   head "https://github.com/ind4skylivey/matteria-track.git", branch: "main"
 
@@ -25,7 +25,7 @@ class Materiatrack < Formula
 
   def caveats
     <<~EOS
-      💎 MateriaTrack has been installed!
+      💎 MatteriaTrack has been installed!
 
       Quick start:
         mtrack track -p "Project" -t "Task"  # Start tracking
